36acbc3f352d684fd0831dffef6288f0e461e111
José Alfredo Arroyo Gómez <arroyo.alfredo.1pm@gmail.com>
2023-08-06 07:30:26 -0600

    edit settings

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -82,13 +82,10 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+    'default': 
         dj_database_url.config(
             default='postgresql://postgres:postgresql@localhost/postgres', conn_max_age=600
         )
-    }
 }
 
 
